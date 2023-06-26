3cedef39783f40c669d6a9eef573efd14c342f3c
omar3ain <omar3ain@gmail.com>
2023-06-26 10:41:35 +0300

    resloved the conficts in main

--- Main.js
+++ Main.js
@@ -1,6 +1,10 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Logo from './screens/Logo';
+import InBoardingOne from './screens/inBoarding/InBoardingOne';
+import InBoardingTwo from './screens/inBoarding/inBoardingTwo';
+import InBoardingThree from './screens/inBoarding/inBoardingThree';
+import ExpertMain from './screens/Expert_Advice/Main/Main'
 
 const Stack = createStackNavigator();
 
@@ -9,7 +13,12 @@ export default function Main() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Logo">
         <Stack.Screen name="Logo" component={Logo} options={{ headerShown: false }} />
-        <Stack.Screen name="In boarding" component={Logo} options={{ headerShown: false }} />
+        <Stack.Screen name="In boarding one" component={InBoardingOne} options={{ headerShown: false }} />
+        <Stack.Screen name="In boarding two" component={InBoardingTwo} options={{ headerShown: false }} />
+        <Stack.Screen name="In boarding three" component={InBoardingThree} options={{ headerShown: false }} />
+
+        {/* Expert Advice Screens */}
+        <Stack.Screen name="ExpertMain" component={ExpertMain} options={{ headerShown: false, title: 'Expert Advisor' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/Logo.js
+++ screens/Logo.js
@@ -30,7 +30,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
     padding:20
   },
   logo: {
