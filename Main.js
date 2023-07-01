323c819b16fffc2b0753e88dbe82284804fb48f7
Ahmed Samy <ahmedsamyy1998@gmail.com>
2023-07-01 03:51:31 +0300

    Adding features to hotels

--- Main.js
+++ Main.js
@@ -4,6 +4,7 @@ import Auth from './navigation/Auth';
 import ExpertAdvice from './navigation/Expert_Advice'
 import Profile from './navigation/Profile';
 import InBoarding from './navigation/InBoarding';
+import Hotels from './navigation/Hotels';
 
 const Stack = createStackNavigator();
 
@@ -11,10 +12,11 @@ export default function Main() {
   return (
     <NavigationContainer>
       <Stack.Navigator >
-        <Stack.Screen name="In boarding" component={InBoarding} options={{ headerShown: false }} />
+        {/* <Stack.Screen name="In boarding" component={InBoarding} options={{ headerShown: false }} />
         <Stack.Screen name="Auth" component={Auth} options={{ headerShown: false }} />
-        <Stack.Screen name="Profile" component={Profile} options={{ headerShown: false }} />
-        <Stack.Screen name="ExpertAdvice" component={ExpertAdvice} options={{ headerShown: false }} />
+        <Stack.Screen name="Profile" component={Profile} options={{ headerShown: false }} /> */}
+        {/* <Stack.Screen name="ExpertAdvice" component={ExpertAdvice} options={{ headerShown: false }} /> */}
+        <Stack.Screen name="Hotels" component={Hotels} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
