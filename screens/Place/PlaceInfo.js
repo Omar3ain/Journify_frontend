4a10a3cc24208b350bdcb52aab006a2e212ac237
MarwaAbdelAal <marwaabdelaal112@gmail.com>
2023-07-04 03:54:11 +0300

    fixed unfound image source

--- screens/Place/PlaceInfo.js
+++ screens/Place/PlaceInfo.js
@@ -56,7 +56,11 @@ export default function PlaceInfo() {
   return (
     <View style={styles.container}>
       <View style={styles.card}>
-        <Image source={{ uri: responseData.preview.source || '' }} style={styles.image} />
+        <Image source={{
+          uri: responseData.preview
+          ? responseData.preview.source
+          : "https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=",
+          }} style={styles.image} />
         <View style={styles.details}>
           <Text style={styles.name}>{responseData.name}</Text>
   
